b72d3daf7a7c405c7b76c424db31c57925c3313b
aliaksei <aliaksmarchuk@gmail.com>
2025-06-09 17:37:52 +0200

    add migrations

---
+++ Migrations/20250609145941_09_06_2025_17_00.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Allexgro.Migrations
+{
+    /// <inheritdoc />
+    public partial class _09_06_2025_17_00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAttributes_Products_ProductId",
+                table: "ProductAttributes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductAttributes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAttributes_Products_ProductId",
+                table: "ProductAttributes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAttributes_Products_ProductId",
+                table: "ProductAttributes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "ProductAttributes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAttributes_Products_ProductId",
+                table: "ProductAttributes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
