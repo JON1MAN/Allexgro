6d4671886efbdb7441b77169e2d138c172b78ec7
aliaksei <aliaksmarchuk@gmail.com>
2025-06-17 17:59:13 +0200

    possible null values

--- search/controller/mapper/FilterDTO.cs
+++ search/controller/mapper/FilterDTO.cs
@@ -1,9 +1,9 @@
 
 public class FilterDTO
 {
-    public Decimal minPrice { get; set; }
-    public Decimal maxPrice { get; set; }
-    public int ProductCategoryId { get; set; }
-    public int ProductTypeId { get; set; }
-    public ICollection<ProductAttributeDTO> ProductAttributes { get; set; }
+    public Decimal? minPrice { get; set; }
+    public Decimal? maxPrice { get; set; }
+    public int? ProductCategoryId { get; set; }
+    public int? ProductTypeId { get; set; }
+    public ICollection<ProductAttributeDTO>? ProductAttributes { get; set; }
 }
